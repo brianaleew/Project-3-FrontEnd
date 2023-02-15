d5820bcd8dd96503572310430fcdb84de00d8642
Eric Elsner <ericelsnercodes@gmail.com>
2023-02-15 13:16:35 -0500

    updated ShowGallery page

--- src/App.js
+++ src/App.js
@@ -16,86 +16,117 @@ import ShowGallery from './components/galleries/ShowGallery'
 import CreateGallery from './components/galleries/CreateGallery'
 
 const App = () => {
+    const [user, setUser] = useState(null)
+    const [msgAlerts, setMsgAlerts] = useState([])
 
-  const [user, setUser] = useState(null)
-  const [msgAlerts, setMsgAlerts] = useState([])
+    console.log('user in app', user)
+    console.log('message alerts', msgAlerts)
+    const clearUser = () => {
+        console.log('clear user ran')
+        setUser(null)
+    }
 
-  console.log('user in app', user)
-  console.log('message alerts', msgAlerts)
-  const clearUser = () => {
-    console.log('clear user ran')
-    setUser(null)
-  }
+    const deleteAlert = id => {
+        setMsgAlerts(prevState => {
+            return prevState.filter(msg => msg.id !== id)
+        })
+    }
 
-	const deleteAlert = (id) => {
-		setMsgAlerts((prevState) => {
-			return (prevState.filter((msg) => msg.id !== id) )
-		})
-	}
+    const msgAlert = ({ heading, message, variant }) => {
+        const id = uuid()
+        setMsgAlerts(() => {
+            return [{ heading, message, variant, id }]
+        })
+    }
 
-	const msgAlert = ({ heading, message, variant }) => {
-		const id = uuid()
-		setMsgAlerts(() => {
-			return (
-				[{ heading, message, variant, id }]
-      )
-		})
-	}
+    return (
+        <Fragment>
+            <Header user={user} />
+            <Routes>
+                <Route
+                    path='/'
+                    element={
+                        <Home
+                            msgAlert={msgAlert}
+                            user={user}
+                        />
+                    }
+                />
+                <Route
+                    path='/sign-up'
+                    element={
+                        <SignUp
+                            msgAlert={msgAlert}
+                            setUser={setUser}
+                        />
+                    }
+                />
+                <Route
+                    path='/sign-in'
+                    element={
+                        <SignIn
+                            msgAlert={msgAlert}
+                            setUser={setUser}
+                        />
+                    }
+                />
+                <Route
+                    path='/sign-out'
+                    element={
+                        <RequireAuth user={user}>
+                            <SignOut
+                                msgAlert={msgAlert}
+                                clearUser={clearUser}
+                                user={user}
+                            />
+                        </RequireAuth>
+                    }
+                />
+                <Route
+                    path='/change-password'
+                    element={
+                        <RequireAuth user={user}>
+                            <ChangePassword
+                                msgAlert={msgAlert}
+                                user={user}
+                            />
+                        </RequireAuth>
+                    }
+                />
 
-		return (
-			<Fragment>
-				<Header user={user} />
-				<Routes>
-					<Route path='/' element={<Home msgAlert={msgAlert} user={user} />} />
-					<Route
-						path='/sign-up'
-						element={<SignUp msgAlert={msgAlert} setUser={setUser} />}
-					/>
-					<Route
-						path='/sign-in'
-						element={<SignIn msgAlert={msgAlert} setUser={setUser} />}
-					/>
-          <Route
-            path='/sign-out'
-            element={
-              <RequireAuth user={user}>
-                <SignOut msgAlert={msgAlert} clearUser={clearUser} user={user} />
-              </RequireAuth>
-            }
-          />
-          <Route
-            path='/change-password'
-            element={
-              <RequireAuth user={user}>
-                <ChangePassword msgAlert={msgAlert} user={user} />
-              </RequireAuth>}
-          />
-
-          <Route
-					path='/create-gallery'
-					element={
-					<RequireAuth user={user}>
-						<CreateGallery msgAlert={msgAlert} user={user} />
-					</RequireAuth>}
-				/>
-				<Route 
-					path='galleries/:id'
-					element={ <ShowGallery user={user} msgAlert={msgAlert} />}
-				/>
-
-				</Routes>
-				{msgAlerts.map((msgAlert) => (
-					<AutoDismissAlert
-						key={msgAlert.id}
-						heading={msgAlert.heading}
-						variant={msgAlert.variant}
-						message={msgAlert.message}
-						id={msgAlert.id}
-						deleteAlert={deleteAlert}
-					/>
-				))}
-			</Fragment>
-		)
+                <Route
+                    path='/create-gallery'
+                    element={
+                        <RequireAuth user={user}>
+                            <CreateGallery
+                                msgAlert={msgAlert}
+                                user={user}
+                            />
+                        </RequireAuth>
+                    }
+                />
+                <Route
+                    path='galleries/:id'
+                    element={
+                        <ShowGallery
+                            user={user}
+                            msgAlert={msgAlert}
+                        />
+                    }
+                />
+            </Routes>
+            {msgAlerts.map(msgAlert => (
+                <AutoDismissAlert
+                    key={msgAlert.id}
+                    heading={msgAlert.heading}
+                    variant={msgAlert.variant}
+                    message={msgAlert.message}
+                    id={msgAlert.id}
+                    deleteAlert={deleteAlert}
+                />
+            ))}
+        </Fragment>
+    )
 }
 
 export default App
