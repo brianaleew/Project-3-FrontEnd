a7557aba2316951878383861e3201576a2476010
Eric Elsner <ericelsnercodes@gmail.com>
2023-02-16 08:28:38 -0500

    moar landing page styling and sign in page styling

--- src/components/galleries/GalleryIndex.js
+++ src/components/galleries/GalleryIndex.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { getAllGalleries } from '../../api/gallery'
 import messages from '../shared/AutoDismissAlert/messages'
+import { FiImage, FiCompass } from 'react-icons/fi'
 
 import './GalleryIndex.css'
 
@@ -26,7 +27,7 @@ const GalleryIndex = props => {
     console.log(galleriesA)
     const galleryArray = galleriesA.map(gallery => (
         <li key={gallery._id}>
-            <h5>{gallery.name}</h5>
+            <p>{gallery.name}</p>
             <img
                 className='gallery-index__gallery-image'
                 src={'/art-gallery.jpg'}
@@ -38,6 +39,7 @@ const GalleryIndex = props => {
     console.log(galleryArray)
 
     return (
+<<<<<<< HEAD
         <div>
 <<<<<<< HEAD
         <img src =" "></img>
@@ -52,8 +54,34 @@ const GalleryIndex = props => {
                 src={'/art-gallery.jpg'}
                 alt='hero'
             />
+=======
+        <div className='main'>
+            <div className='gallery-index__hero'>
+                <img
+                    className='gallery-index__hero-image'
+                    src={'/art-gallery.jpg'}
+                    alt='hero'
+                />
+                <div className='gallery-index__hero-icons gh-flex-row'>
+                    <div className='gh-flex-clm-c'>
+                        <FiCompass
+                            // color='white'
+                            size='5rem'
+                        />
+                        <p>explore</p>
+                    </div>
+                    <div className='gh-flex-clm-c'>
+                        <FiImage
+                            // color='white'
+                            size='5rem'
+                        />
+                        <p>curate</p>
+                    </div>
+                </div>
+            </div>
+>>>>>>> bcd5f65 (moar landing page styling and sign in page styling)
             <div className='gh-main-container'>
-                <h4>recommended galleries</h4>
+                <p>recommended galleries</p>
                 <ul className='gh-grid-400'>{galleryArray}</ul>
 >>>>>>> bf30351 (landing page style part 1)
             </div>
