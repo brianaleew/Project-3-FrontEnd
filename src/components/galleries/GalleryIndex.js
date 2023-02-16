f0bae52de09982f4511a7576fb49a262243f8e2d
blkurd <blkurd@hotmail.com>
2023-02-16 14:13:08 -0500

    changes

--- src/components/galleries/GalleryIndex.js
+++ src/components/galleries/GalleryIndex.js
@@ -2,18 +2,6 @@ import { useState, useEffect } from 'react'
 import { getAllGalleries } from '../../api/gallery'
 import messages from '../shared/AutoDismissAlert/messages'
 import { FiImage, FiCompass } from 'react-icons/fi'
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-import './GalleryIndex.css'
-=======
->>>>>>> bcd5f65 (moar landing page styling and sign in page styling)
-=======
->>>>>>> 23e419c (moar landing page styling and sign in page styling)
-
-import './GalleryIndex.css'
-=======
->>>>>>> a7557ab (moar landing page styling and sign in page styling)
 
 import './GalleryIndex.css'
 
@@ -39,31 +27,7 @@ const GalleryIndex = props => {
     console.log(galleriesA)
     const galleryArray = galleriesA.map(gallery => (
         <li key={gallery._id}>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
             <p>{gallery.name}</p>
-=======
-            <h5>{gallery.name}</h5>
->>>>>>> bf30351 (landing page style part 1)
-=======
-            <p>{gallery.name}</p>
->>>>>>> bcd5f65 (moar landing page styling and sign in page styling)
-=======
-=======
->>>>>>> 23e419c (moar landing page styling and sign in page styling)
-            <p>{gallery.name}</p>
-=======
-            <h5>{gallery.name}</h5>
->>>>>>> c6bc717 (landing page style part 1)
-<<<<<<< HEAD
->>>>>>> 5c47a7c (landing page style part 1)
-=======
-=======
-            <p>{gallery.name}</p>
->>>>>>> a7557ab (moar landing page styling and sign in page styling)
->>>>>>> 23e419c (moar landing page styling and sign in page styling)
             <img
                 className='gallery-index__gallery-image'
                 src={'/art-gallery.jpg'}
@@ -75,86 +39,6 @@ const GalleryIndex = props => {
     console.log(galleryArray)
 
     return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> bcd5f65 (moar landing page styling and sign in page styling)
-=======
->>>>>>> 5c47a7c (landing page style part 1)
-=======
->>>>>>> 23e419c (moar landing page styling and sign in page styling)
-=======
->>>>>>> bde1393 (ll)
-=======
->>>>>>> 1b75844 (Updated messages + gallery show and edit pages)
-        <div className='main'>
-            <div className='gallery-index__hero'>
-                <img
-                    className='gallery-index__hero-image'
-                    src={'/art-gallery.jpg'}
-                    alt='hero'
-                />
-                <div className='gallery-index__hero-icons gh-flex-row'>
-                    <div className='gh-flex-clm-c'>
-                        <FiCompass
-                            // color='white'
-                            size='5rem'
-                        />
-                        <p>explore</p>
-                    </div>
-                    <div className='gh-flex-clm-c'>
-                        <FiImage
-                            // color='white'
-                            size='5rem'
-                        />
-                        <p>curate</p>
-                    </div>
-                </div>
-            </div>
-<<<<<<< HEAD
-            <div className='gh-main-container'>
-                <p>recommended galleries</p>
-=======
-        <div>
-            <img
-                className='gallery-index__hero-image'
-                src={'/art-gallery.jpg'}
-                alt='hero'
-            />
-            <div className='gh-main-container'>
-                <h4>recommended galleries</h4>
->>>>>>> bf30351 (landing page style part 1)
-=======
-            <div className='gh-main-container'>
-                <p>recommended galleries</p>
->>>>>>> bcd5f65 (moar landing page styling and sign in page styling)
-                <ul className='gh-grid-400'>{galleryArray}</ul>
-=======
-=======
->>>>>>> a7557ab (moar landing page styling and sign in page styling)
-=======
->>>>>>> aa1735c (ll)
-        <div>
-<<<<<<< HEAD
-        <img src =" "></img>
-        <div>
-
-            <h1>Recommended Galleries</h1>
-
-                <ul>{galleryArray}</ul>
-=======
-            <img
-                className='gallery-index__hero-image'
-                src={'/art-gallery.jpg'}
-                alt='hero'
-            />
-=======
-=======
->>>>>>> 4834e91 (Updated messages + gallery show and edit pages)
         <div className='main'>
             <div className='gallery-index__hero'>
                 <img
@@ -182,23 +66,6 @@ const GalleryIndex = props => {
             <div className='gh-main-container'>
                 <p>recommended galleries</p>
                 <ul className='gh-grid-400'>{galleryArray}</ul>
-<<<<<<< HEAD
->>>>>>> bf30351 (landing page style part 1)
-<<<<<<< HEAD
->>>>>>> c6bc717 (landing page style part 1)
-=======
-=======
-        <div>
-        <img src =" "></img>
-        <div>
-
-            <h1>Recommended Galleries</h1>
-
-                <ul>{galleryArray}</ul>
->>>>>>> ea1d367 (ll)
->>>>>>> aa1735c (ll)
-=======
->>>>>>> 4834e91 (Updated messages + gallery show and edit pages)
             </div>
         </div>
     )
--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -4,40 +4,9 @@ import './Footer.css'
 
 const Footer = props => {
     return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 5c47a7c (landing page style part 1)
-=======
->>>>>>> 23e419c (moar landing page styling and sign in page styling)
         <div className='footer gh-flex-row-c'>
             <FaRegCopyright />
             <p>Hugz4Bugz</p>
-=======
-        <div className='footer'>
-            <p>
-                <FaRegCopyright /> Hugz4Bugz
-            </p>
-<<<<<<< HEAD
->>>>>>> bf30351 (landing page style part 1)
-=======
-        <div className='footer gh-flex-row-c'>
-            <FaRegCopyright />
-            <p>Hugz4Bugz</p>
->>>>>>> bcd5f65 (moar landing page styling and sign in page styling)
-=======
->>>>>>> c6bc717 (landing page style part 1)
-<<<<<<< HEAD
->>>>>>> 5c47a7c (landing page style part 1)
-=======
-=======
-        <div className='footer gh-flex-row-c'>
-            <FaRegCopyright />
-            <p>Hugz4Bugz</p>
->>>>>>> a7557ab (moar landing page styling and sign in page styling)
->>>>>>> 23e419c (moar landing page styling and sign in page styling)
         </div>
     )
 }
