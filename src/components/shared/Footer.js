bf30351ef647d531eb707d628cbbf22fd39751e9
Eric Elsner <ericelsnercodes@gmail.com>
2023-02-15 21:13:12 -0500

    landing page style part 1

---
+++ src/components/shared/Footer.js
@@ -0,0 +1,15 @@
+import { FaRegCopyright } from 'react-icons/fa'
+
+import './Footer.css'
+
+const Footer = props => {
+    return (
+        <div className='footer'>
+            <p>
+                <FaRegCopyright /> Hugz4Bugz
+            </p>
+        </div>
+    )
+}
+
+export default Footer
