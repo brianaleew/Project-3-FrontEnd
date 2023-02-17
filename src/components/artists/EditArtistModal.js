f3314c3c22f7cc9674d016a651d7aa6fd7b82e0e
Eric Elsner <ericelsnercodes@gmail.com>
2023-02-16 19:22:14 -0500

    fixed the edit artist issue, yay

--- src/components/artists/EditArtistModal.js
+++ src/components/artists/EditArtistModal.js
@@ -1,78 +1,93 @@
 //The EditArtistModal is rendered in the ArtistIndex
 
-import React, { useState } from 'react' 
+import { useState, useEffect } from 'react'
 import { Modal } from 'react-bootstrap'
 import ArtistForm from '../shared/ArtistForm'
 // import { updateArtist } from '../../api/artist'
-import { updateArtistFailure, updateArtistSuccess } from '../shared/AutoDismissAlert/messages'
+import {
+    updateArtistFailure,
+    updateArtistSuccess,
+} from '../shared/AutoDismissAlert/messages'
 
-const EditArtistModal = (props) => {
-    //all the props we need 
-    const { user, show, artist, updateArtist, handleClose, msgAlert, triggerRefresh } = props 
-    
-    const [artistInfo, setArtistInfo] = useState(props.artist)
+const EditArtistModal = props => {
+    //all the props we need
+    const {
+        artist,
+        user,
+        show,
+        updateArtist,
+        handleClose,
+        msgAlert,
+        triggerRefresh,
+    } = props
+
+    const [artistInfo, setArtistInfo] = useState(artist)
+
+    useEffect(() => {
+        setArtistInfo(artist)
+        console.log('ARTISTINFO ', artistInfo)
+    }, [])
+
     //linking names and values to what is input by users in form
-    const onChange = (e) => {
-        e.persist()
-
+    const onChange = e => {
         setArtistInfo(prevArtist => {
-            const updatedName = e.target.name 
-            let updatedValue = e.target.value 
+            e.preventDefault()
+            const updatedName = e.target.name
+            let updatedValue = e.target.value
 
             const updatedArtist = {
-                [updatedName] : updatedValue
+                [updatedName]: updatedValue,
             }
 
             return {
-                ...prevArtist, ...updatedArtist
+                ...prevArtist,
+                ...updatedArtist,
             }
         })
     }
 
-
-    const onSubmit = (e) => {
+    const onSubmit = e => {
         e.preventDefault()
 
         updateArtist(user, artistInfo)
             //closing modal
-            .then(() => handleClose()) 
+            .then(() => handleClose())
             //sending user the success message
             .then(() => {
                 msgAlert({
                     heading: 'Nice Edit!',
                     message: updateArtistSuccess,
-                    variant: 'success'
+                    variant: 'success',
                 })
             })
-            //refreshing 
+            //refreshing
             .then(() => triggerRefresh())
             //handling error with message to user
             .catch(() => {
                 msgAlert({
                     heading: 'Oops!',
                     message: updateArtistFailure,
-                    variant: 'danger'
+                    variant: 'danger',
                 })
             })
     }
 
-    console.log('this is artistInfo', artistInfo._id)
+    // console.log('this is artistInfo in the modal', artistInfo._id)
     return (
         <>
-        <Modal show={show} onHide={handleClose}>
-
-            <Modal.Body>
-                <ArtistForm
-                    artist={artistInfo}
-                    handleChange={onChange} 
-                    handleSubmit={onSubmit}
-                    heading="Update The Artist"
-                />
-            </Modal.Body>
-        </Modal>
-        
-        
-        
+            <Modal
+                show={show}
+                onHide={handleClose}
+            >
+                <Modal.Body>
+                    <ArtistForm
+                        artist={artistInfo}
+                        handleChange={onChange}
+                        handleSubmit={onSubmit}
+                        heading='Update The Artist'
+                    />
+                </Modal.Body>
+            </Modal>
         </>
     )
 }
